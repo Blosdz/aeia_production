b27e6fc6e62976cb6cade141ff36b065aa24914e
johan <johan.jimenez@ucsp.edu.pe>
2024-08-01 12:33:43 -0500

    layout

--- resources/views/users_new/index.blade.php
+++ resources/views/users_new/index.blade.php
@@ -1,8 +1,10 @@
 @extends('layouts_new.app')
 
 @section('content')
-    <div class="container_dashboard_background" id="contracts_table">
-        <div class="dashboard-new-title" >Suscriptores / Clientes / Business</div>
+
+<strong>Suscriptores / Clientes / Business</strong>
+
+    <div class="row">
         <div class="animated fadeIn">
              @include('flash::message')
             <div class="row" id="contracts-row-1">
