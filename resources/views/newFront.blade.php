d68197fe201113c68c7267c9ef6a79857f5dba01
johan <johan.jimenez@ucsp.edu.pe>
2024-10-04 10:03:18 -0500

    currencies_app_nav

--- resources/views/newFront.blade.php
+++ resources/views/newFront.blade.php
@@ -21,7 +21,7 @@
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
               <span class="navbar-toggler-icon"></span>
             </button>
-            <div class="navbar-collapse collapse justify-content-end d-flex " id="navbarSupportedContent ">
+            <div class=" collapse justify-content-end d-flex " id="navbarSupportedContent ">
                 <ul class=" navbar-nav me-auto mb-2 mb-lg-0 me-auto mb-2 mb-lg-0">
 
                     <li class="nav-f d-flex justify-content-center align-items-center text-align-center nav-item">
