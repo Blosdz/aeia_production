0acee30ef699b2eaca149f96295e0b52e8a71941
johan <johan.jimenez@ucsp.edu.pe>
2024-12-04 10:03:23 -0500

    routes_fix

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -17,16 +17,34 @@ class RedirectIfAuthenticated
      * @param  string|null  ...$guards
      * @return mixed
      */
-    public function handle(Request $request, Closure $next, ...$guards)
-    {
-        $guards = empty($guards) ? [null] : $guards;
 
+     public function handle(Request $request, Closure $next, ...$guards)
+    {
         foreach ($guards as $guard) {
             if (Auth::guard($guard)->check()) {
-                return redirect(RouteServiceProvider::HOME);
+                $role = Auth::user()->rol;
+            
+                switch ($role) {
+                    case 1:
+                        return redirect('/admin/home');
+                    case 2:
+                        return redirect('/suscriptor/home');
+                    case 3:
+                    case 4:
+                        return redirect('/user/home');
+                    case 5:
+                        return redirect('/gerente/home');
+                    case 6:
+                        return redirect('/verificador/home');
+                    case 8:
+                        return redirect('/banco/home');
+                    default:
+                        return redirect('/start'); // Ruta por defecto.
+                }
             }
         }
-
+    
         return $next($request);
     }
+
 }
