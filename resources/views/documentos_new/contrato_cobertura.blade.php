c49ee79ca292a0e9ef1d4245a650ff80fde6deca
johan <johan.jimenez@ucsp.edu.pe>
2025-01-07 12:57:54 -0500

    Descripción de los cambios realizados

---
+++ resources/views/documentos_new/contrato_cobertura.blade.php
@@ -0,0 +1,176 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Cobertura</title>
+        <link href="css/style.css" rel="stylesheet">
+
+    </head>
+    <body>
+    <style>
+        ol { counter-reset: item }
+        li{ display: block }
+        ol > li:before { content: counters(item, ".") " "; counter-increment: item }
+    </style>
+    <div style="width:100%; text-align:center"><u><b>Contrato de seguro deportivo </b></u></div>
+    <p><u><b>Partes del Contrato</b></u></p><br>
+    <p><u><b>Fondo de Cobertura deportiva: </b></u></p><br>
+    <table>
+        <tr>
+            <td>
+                <ul>
+                    <li><b>Fondo de Inversión</b>: AEIA INVESTMENTS E.I.R.L</li>
+                    <li><b>RUC</b>: 20608381741</li>
+                    <li><b>WhatsApp</b>+51 944877217</li>
+                    <li><b>Página Web</b>: AEIA.CAPITAL</li>
+                </ul>
+            </td>
+        </tr>
+    </table>
+    <br>
+    <p><u><b>Asegurado: </b></u></p><br>
+    <table>
+        <tr>
+            <td>
+                <ul>
+                    <li><b>Nombre del Padre/Tutor:</b></li>
+                    <li><b>DNI Nombre del Padre/Tutor:</b></li>
+                    <li><b>Dirección:</b></li>
+                    <li><b>Teléfono:</b></li>
+                    <li><b>Correo Electrónico:</b></li>
+                    <li><b>Nombre del Beneficiario:</b></li>
+                    <li><b>DNI del Beneficiario:</b></li>
+                    <li><b>Edad del Beneficiario:</b></li>
+                </ul>
+            </td>
+        </tr>
+    </table><br>
+    <p><u><b>Objeto del contrato </b></u></p><br>
+    <p>
+    El presente contrato tiene por objeto proporcionar cobertura económica al beneficiario en caso de lesiones deportivas sufridas durante la práctica de actividades deportivas.
+    </p>
+    <p><u><b>Condiciones Generales </b></u></p><br>
+    <table>
+        <tr>
+            <ol>
+                <li><b>Cobertura y Limitaciones</b>
+                <ol>
+                    <li><b>Cobertura Máxima: </b>Hasta S/.1000.00 soles por único evento anual. Si el costo total de la cobertura médica es menor a S/.1000.00 soles, no se generará ningún saldo a favor del beneficiario, ni acumulación de cuotas. </li>
+                    <li><b>Número de Eventos Cubiertos: </b>Máximo de 1 evento cubierto por año.</li>
+                    <li><b>Detalle de la atención: </b>Atención por emergencia, cita médica, exámenes complementarios, receta médica, controles médicos, hasta 3 sesiones de rehabilitación física. En caso de que el usuario sufra alguna negligencia médica AEIA no se hará responsable ya que el usuario el libre de elegir el tipo de atención que requiera su lesión.</li>
+                    <li><b>Tipo de pago: </b>
+                    <ul>
+                        <li><b>Pago anual: </b>S/.180.00</li>
+                        <li><b>Pago mensual: </b>S/.15.00</li>
+                    </ul>
+                        <b>Únicamente Transferencia Bancaria</b> a la cuenta simple en soles de <b>INTERBANK</b> a nombre de <b>Diego Butrón Núñez</b>, Representante legal de la empresa <b>AEIA Investments con RUC: 20608381741.</b> <br>
+                        N° CTA.: 4623340100022 <br>
+                        N° CCI.: 00346201334010002298 <br>
+                        <b>
+                            No se aceptan depósitos en ventanilla ni pagos en efectivo
+                        </b>
+                    </li>
+                    <li><b>Periodo de Carencia:</b> <br>
+                        <b>Para pagos anuales: </b>La cobertura será efectiva a partir de los 2 meses posteriores a la suscripción del contrato. <br>
+                        <b>Para pagos mensuales: </b>La cobertura será efectiva a partir de los 3 meses posteriores a la suscripción del contrato.
+                    </li>
+                    <li><b>Suspención de cobertura: </b>La cobertura puede ser suspendida automáticamente si no se realiza el pago de la mensualidad, teniendo como periodo máximo de ejecución de pago 1 semana a partir de la fecha de cierre de la mensualidad.</li>
+                    <li><b>Penalidad: </b>En caso de incumplimiento de pago se perderá el periodo de carencia y podrá ser únicamente restituido con el pago se una penalidad con valor de S/.300.00 soles por única vez. </li>
+                    <li><b>Notificación: </b> AEIA Investments notificará al beneficiario sobre el incumplimiento de pago y sus posibles consecuencias a través del WhatsApp y su cuenta de cliente dentro de la página https://aeia.capital</li>
+                    <li><b>Periodo de Contrato: </b>El contrato es anual en cualquiera de sus modalidades por lo que el beneficiario está adquiriendo una obligación de pago ante la empresa AEIA Investments y la empresa asume el compromiso de cobertura</li>
+                    <li><b>Resolución del contrato: </b>Si el beneficiario no realiza el pago de la multa hasta un mes después de la notificación de pago.</li>
+                </ol>
+                </li>
+                <li><b>Lesiones Cubiertas</b>
+                    <ol>
+                        <li><b>Fracturas:</b> Cualquier tipo de fractura ósea ocurrida durante la práctica deportiva. </li>
+                        <li><b>Esguinces y Desgarros:</b> Lesiones de ligamentos y músculos a partir de grado II.</li>
+                        <li><b>Contusiones y Traumatismos:</b> Golpes y traumas a partir de grado II que requieran atención médica.</li>
+                    </ol>
+                </li>
+
+                <li><b>Lesiones No Cubiertas</b>
+                    <ol>
+                        <li><b>Lesiones Crónicas: </b>Tendinitis, bursitis, lumbalgia, fascitis plantar, epicondilitis (Codo de tenista), Síndrome de talón de Aquiles, Síndrome del tendón de la corva, Periostitis tibial.</li>
+                        <li><b>Conducta antideportiva: </b>No se cubrirán lesiones originadas por peleas.</li>
+                        <li><b>Uso de sustancias: </b>No se cubrirán lesiones originadas por el uso de bebidas alcohólicas o sustancias estupefacientes.</li>
+                    </ol>
+                </li>
+                <li><b>Condiciones de Cobertura</b>
+                    <ol>
+                        <li>Las lesiones deben ocurrir durante actividades deportivas, como entrenamientos, competencias, campeonatos, etc.</li>
+                        <li>El beneficiario debe utilizar el equipo de protección adecuado y seguir las normas de seguridad del deporte.</li>
+                        <li>La cobertura está diseñada para deportistas de entre los 5 años y los 18 años.</li>
+                        <li>La cobertura es para eventos deportivos dentro del territorio peruano.</li>
+                        <li>Los menores de edad serán representados por su padre o apoderado.</li>
+                        <li>Solo puede existir un beneficiario por plan de cobertura.</li>
+                        <li>La cobertura no podrá ser transferida.</li>
+                        <li>La cobertura está diseñada para deportistas de las siguientes disciplinas: Futbol, Futsal, Basket, Voley, Balonmano, Tenis, Ping Pong, Frontón, Padel, Natación, Badminton, Atletismo, Running, E-sports, Tiro deportivo, Culturismo Fitness, Bolos, Gimnasia, Breakdance, Surfing, Waterpolo, Golf</li>
+                        <li>La cobertura no contempla deportes como: Cacería, Rugby, Futbol Americano, Beisbol, Deportes De Motor, Ciclismo, Skate, Artes Marciales, Calistenia, Paracaidismo.</li>
+                    </ol>
+                </li>
+                <li><b>Procedimiento de Reclamo</b>
+                    <ol>
+                        <li>
+                            <b>Notificación del Incidente</b>
+                                El padre o tutor del beneficiario debe notificar el incidente al Fondo de cobertura de AEIA Investments dentro de un plazo de 24 horas desde el momento de la lesión a través de nuestro canal de WhatsApp con número +51 944877217.
+                            <br>
+                        </li>
+                        <li>
+                            <b>Documentación Requerida</b>
+
+                            <br>
+                            Enviar a través del canal de atención de WhatsApp los siguientes documentos:
+                        </li>
+                    </ol>
+                        <ul>
+                            <li><b>Informe médico:</b> Detallado que incluya diagnóstico, tratamiento recomendado y receta médica. </li>
+                            <li><b>Certificación del evento:</b> Un documento que certifique que la lesión ocurrió durante una actividad deportiva autorizada (por ejemplo, una carta del entrenador o de la institución deportiva).
+                            <b> En caso de no poder obtener este certificado, se aceptarán fotografías del momento del incidente y una declaración escrita del padre, tutor o entrenador describiendo las circunstancias del evento.</b>
+                            </li>
+                            <li><b>Radiografías y exámenes externos:</b> Cualquier radiografía o examen médico que corrobore la lesión.</li>
+                            <li><b>Boletas o Facturas de Servicios Médicos:</b> Comprobantes de pago de los gastos médicos en los que se ha incurrido. <br>
+                            <br>
+                                No se aceptan (YAPES, recibos por honorarios, ni gastos por concepto de movilidades).<br>                                <br>
+                                Se aceptan radiografías, ecografías, resonancias, tomografías, consultas médicas, gatos de farmacias, yesos, hospitalización, hasta 3 sesiones de rehabilitación o fisioterapia, operaciones; relacionadas con la lesión reportada.
+                            <br>
+                            </li>
+                        </ul>
+                    Una vez enviada la documentación será evaluada por el personal de AEIA Investments y se realizará la comunicación pertinente con el titular de la cuenta.
+                </li>
+                <li><b>Evaluación del Reclamo</b>
+                    <ul>
+                        <li>Toda la documentación será presentada a través de WhatsApp +51 944877217, y debe ser verdadera. En caso de que se detecten boletas o documentos alterados, el beneficiario perderá su cobertura y se iniciarán las acciones legales correspondientes. </li>
+                        <li>Confirmación de la elegibilidad de la lesión y cálculo del monto a ser reembolsado de acuerdo a los documentos presentados.</li>
+                        <li>Como plazo máximo de evaluación serán 48 horas para aprobar o rechazar el reclamo.</li>
+                    </ul>
+                </li>
+                <li><b>Aprobación y Pago </b>
+                    <ul>
+                        <li>Notificación de aprobación del reclamo al titular de la cuenta. </li>
+                        <li>Desembolso del pago a través de transferencia bancaria directamente al titular de la cuenta a partir de las 72 horas a partir de la validación del expediente.</li>
+                        <li>No se aceptarán pagos en efectivo, cuentas de terceros o Yape.</li>
+                        <li>El desembolso de dinero será una sola vez por el monto calculado en el expediente de reclamo por lo que una vez presentado no se incluirán gastos adicionales a los presentados.</li>
+
+
+                    </ul>
+                </li>
+                <li><b>Renovación del contrato</b>
+                    <ol>
+                        <li><b>Finalización y Renovación:</b>Al finalizar el periodo de cobertura anual, el contrato se renovará automáticamente por un año adicional, a menos que el contratante notifique por escrito su intención de no renovar al menos 30 días antes de la fecha de vencimiento.  </li>
+                        <li><b>Recordatorio:</b>AEIA Investments enviará un recordatorio al padre o tutor del beneficiario a través de correo electrónico y WhatsApp, 30 días antes de la fecha de renovación, informando sobre la renovación automática y el monto a pagar. </li>
+                        <li><b>Modificaciones</b> En caso de que el contratante desee realizar cambios en la cobertura o en los datos del beneficiario, deberá comunicarlo durante el periodo de notificación mencionado anteriormente.  </li>
+                    </ol>
+                </li>
+
+            </ol>
+
+        </tr>
+
+    </table>
+
+
+    </body>
+</html>
+
